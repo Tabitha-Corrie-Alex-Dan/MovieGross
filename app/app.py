07777fa9940b7b2991a64decde7ecbdb5caa8314
Alex Martinez <alexmartinez1745@gmail.com>
2021-04-24 12:26:43 -0400

    working on flask app

--- app/app.py
+++ app/app.py
@@ -1,13 +1,12 @@
 # Import libraries
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scraper
 
 # Create Flask app
 app = Flask(__name__)
 
 # PyMongo for Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/movieseasons")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/imdb_DB")
 
 
 
