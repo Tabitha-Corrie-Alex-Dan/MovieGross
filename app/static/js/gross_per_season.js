ba75e1356e8db80a703521aa0e2b447aa5869e9e
Alex Martinez <alexmartinez1745@gmail.com>
2021-04-28 12:59:32 -0400

    working on grabbing data for bar chart

--- app/static/js/gross_per_season.js
+++ app/static/js/gross_per_season.js
@@ -1,4 +1,32 @@
-d3.json('/sum').then(function(getSum) {
-    console.log(getSum)
+d3.json("/sum").then(function (getSum) {
+  console.log(getSum);
 
-})
+  var season = getSum.season;
+  var cumGross = getSum.cum_gross;
+  var average = getSum.average;
+
+  var trace1 = {
+    type: "bar",
+    x: season,
+    y: cumGross,
+    line: {
+      color: "#17BECF",
+    },
+  };
+
+
+  var data = [trace1];
+
+  var layout = {
+    title: "Gross per season",
+    xaxis: {
+      range: [length.season],
+    },
+    yaxis: {
+      autorange: true,
+      type: "linear",
+    },
+  };
+
+  Plotly.newPlot("plot", data, layout);
+});
