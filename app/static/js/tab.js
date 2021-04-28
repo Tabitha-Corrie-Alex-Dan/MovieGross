ca4d5c4072767cfcc960a6dd6a501f6545b63cdc
Tabitha Head <tabithahead@Tabithas-MacBook-Pro.local>
2021-04-28 12:32:52 -0400

    fixing plot

--- app/static/js/tab.js
+++ app/static/js/tab.js
@@ -5,18 +5,18 @@ d3.json('/sum').then(function(getData) {
 var data = [
     {
       "type": "sunburst",
-      "labels": ["Fall <br> Cumulative Gross:", "Winter <br> Cumulative Gross:", "Spring <br> Cumulative Gross:", "Summer <br> Cumulative Gross:", "Holiday Season <br> Cumulative Gross:"],
-      "parents": ["", "", "", "", "", "Fall <br> Releases: 5045"],
-      "values":  [21243273.89, 29360349.01, 28614314.82, 78360402.36, 38269909.44, 5045],
+      "labels": ["Fall <br> Cumulative Gross <br> {in dollars}:", "Winter <br> Cumulative Gross <br> {in dollars}:", "Spring <br> Cumulative Gross <br> {in dollars}:", "Summer <br> Cumulative Gross <br> {in dollars}:", "Holiday Season <br> Cumulative Gross <br> {in dollars}:"],
+      "parents": ["", "", "", "", "", ""],
+      "values":  ["21243273.89", "29360349.01", "28614314.82", "78360402.36", "38269909.44"],
       "leaf": {"opacity": 0.4},
       "marker": {"line": {"width": 2}},
-      "branchvalues": 'total'
+      "branchvalues": 'relative'
     }];
     
     var layout = {
       "margin": {"l": 0, "r": 0, "b": 0, "t": 0},
-      sunburstcolorway:["#B22222", "#FF4500", "#FFA500", "#6B8E23", "#4169E1"],
+      sunburstcolorway:["red", "orange", "yellow", "green", "blue"],
     };
     
     
-    Plotly.newPlot("plot", data, layout)
+    Plotly.newPlot('plot', data, layout)
