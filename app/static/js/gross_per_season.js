c8b0216c9a3402068c5435a08ce63e2d23b91006
Alex Martinez <alexmartinez1745@gmail.com>
2021-04-28 21:10:26 -0400

    adding styling to bar chart

--- app/static/js/gross_per_season.js
+++ app/static/js/gross_per_season.js
@@ -3,7 +3,7 @@ d3.json("/sum").then(function (getSum) {
 
   let season = getSum.map((season) => season.season);
   var cumGross = getSum.map((gross) => gross.cum_gross);
-  var average = getSum.map((avg) => avg.average);
+//   var average = getSum.map((avg) => avg.average);
   console.log(season);
   console.log(cumGross)
 
@@ -11,32 +11,30 @@ d3.json("/sum").then(function (getSum) {
     x: season,
     y: cumGross,
     type: 'bar',
-    name: 'Cummlative Gross/Season',
+    name: 'Cumulative Gross/Season',
     marker: {
-      color: 'rgb(49,130,189)',
-      opacity: 0.7,
+      color: ['orange', 'red', 'green', 'yellow', 'blue'],
+      opacity: 0.8,
+      line: {
+        color: 'black',
+        width: 1.2
+      }
     }
   };
   
-  var trace2 = {
-    x: season,
-    y: average,
-    type: 'bar',
-    name: 'Average Gross/Season',
-    marker: {
-      color: 'rgb(204,204,204)',
-      opacity: 0.5
-    }
-  };
-  
-  var data = [trace1, trace2];
+  var data = [trace1];
   
   var layout = {
-    title: 'Gross per Season',
+    yaxis: {
+        title: 'Cumulative Gross (USD)',
+        titlefont: {
+          size: 16,
+          color: 'rgb(107, 107, 107)'
+        }},
     xaxis: {
       tickangle: -45
-    },
-    barmode: 'group'
+    }
+    
   };
   
   Plotly.newPlot('plot', data, layout);
