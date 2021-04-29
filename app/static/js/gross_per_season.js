c9f80eee6b0cbe2deea47688e60e161c2b4bcecc
Alex Martinez <alexmartinez1745@gmail.com>
2021-04-28 20:13:05 -0400

    gross and average chart (test)

--- app/static/js/gross_per_season.js
+++ app/static/js/gross_per_season.js
@@ -3,19 +3,42 @@ d3.json("/sum").then(function (getSum) {
 
   let season = getSum.map((season) => season.season);
   var cumGross = getSum.map((gross) => gross.cum_gross);
-  var average = getSum.average;
+  var average = getSum.map((avg) => avg.average);
   console.log(season);
   console.log(cumGross)
 
-var data = [
-    {
-      x: season,
-      y: cumGross,
-      type: 'bar'
+  var trace1 = {
+    x: season,
+    y: cumGross,
+    type: 'bar',
+    name: 'Cummlative Gross/Season',
+    marker: {
+      color: 'rgb(49,130,189)',
+      opacity: 0.7,
     }
-  ];
+  };
   
-  Plotly.newPlot('plot', data);
+  var trace2 = {
+    x: season,
+    y: average,
+    type: 'bar',
+    name: 'Average Gross/Season',
+    marker: {
+      color: 'rgb(204,204,204)',
+      opacity: 0.5
+    }
+  };
+  
+  var data = [trace1, trace2];
+  
+  var layout = {
+    title: 'Gross per Season',
+    xaxis: {
+      tickangle: -45
+    },
+    barmode: 'group'
+  };
+  
+  Plotly.newPlot('plot', data, layout);
 
 });
-
