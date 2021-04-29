591907fc6015ed340024e1d54e864405d728c011
Alex Martinez <alexmartinez1745@gmail.com>
2021-04-29 15:43:29 -0400

    adding responsive tag for graph

--- app/static/js/gross_per_season.js
+++ app/static/js/gross_per_season.js
@@ -3,41 +3,41 @@ d3.json("/sum").then(function (getSum) {
 
   let season = getSum.map((season) => season.season);
   var cumGross = getSum.map((gross) => gross.cum_gross);
-//   var average = getSum.map((avg) => avg.average);
+  //   var average = getSum.map((avg) => avg.average);
   console.log(season);
-  console.log(cumGross)
+  console.log(cumGross);
 
   var trace1 = {
     x: season,
     y: cumGross,
-    type: 'bar',
-    name: 'Cumulative Gross/Season',
+    type: "bar",
+    name: "Cumulative Gross/Season",
     marker: {
-      color: ['orange', 'red', 'green', 'yellow', 'blue'],
+      color: ["orange", "red", "green", "yellow", "blue"],
       opacity: 0.8,
       line: {
-        color: 'black',
-        width: 1.2
-      }
-    }
+        color: "black",
+        width: 1.2,
+      },
+    },
   };
-  
-  var data = [trace1];
-  
-  var layout = {
-      margin: 0,
-    yaxis: {
-        title: 'Cumulative Gross (USD)',
-        titlefont: {
-          size: 16,
-          color: 'rgb(107, 107, 107)'
-        }},
-    xaxis: {
-      tickangle: -45
-    }
-    
-  };
-  
-  Plotly.newPlot('bar-plot', data, layout);
 
+  var data = [trace1];
+
+  var layout = {
+    title: "Cumulative Gross per Season (2001-2020)",
+    yaxis: {
+      title: "Cumulative Gross (USD)",
+      titlefont: {
+        size: 16,
+        color: "rgb(107, 107, 107)",
+      },
+    },
+    xaxis: {
+      tickangle: -45,
+    },
+  };
+  var config = {responsive: true}
+
+  Plotly.newPlot("bar-plot", data, layout, config);
 });
