8fd5724d0db82328bb0fedb0b65b662822b2e488
Dan <danlarsonfl@gmail.com>
2021-04-28 21:32:50 -0400

    Added chart

--- app/static/js/movieperseason.js
+++ app/static/js/movieperseason.js
@@ -1,21 +1,90 @@
 d3.json('/data').then(function(getData) {
-    console.log(getData)
-})
+    
+  var topRelease = [];
+  var topGross = [];
+  var topReleaseYear = [];
+  var topReleaseSeason = [];  
 
-let season = getSum.map((season) => season.season);
-  var cumGross = getSum.map((gross) => gross.cum_gross);
-  var average = getSum.average;
-  console.log(season);
-  console.log(cumGross)
-
-var data = [
-    {
-      x: season,
-      y: cumGross,
-      type: 'bar'
+  for (var i = 0; i < getData.length; i+5){
+    
+    topRelease.push(getData[i]['num_one_release']);
+    topGross.push(getData[i]['gross']);
+    topReleaseYear.push(getData[i]['year']);
+    topReleaseSeason.push(getData[i]['season']);
     }
-  ];
-  
-  Plotly.newPlot('plot', data);
+    console.log(topRelease);
+    console.log(topGross);
+    console.log(topReleaseYear);
+    console.log(topReleaseSeason);
 
-});
+    // function unpack(rows, key) {
+    //   return rows.map(function(row) { return row[key]; });
+
+    var trace1 = {
+      type: "scatter",
+      mode: "lines",
+      name: 'Holiday Season',
+      x: topRelease,
+      y: topReleaseYear,
+      line: {color: '#17BECF'}
+    }
+    
+    // var trace2 = {
+    //   type: "scatter",
+    //   mode: "lines",
+    //   name: 'Fall',
+    //   x: unpack(rows, 'getData.year'),
+    //   y: unpack(rows, 'getData.gross'),
+    //   line: {color: '#7F7F7F'}
+    // }
+    
+    // var trace3 = {
+    //   type: "scatter",
+    //   mode: "lines",
+    //   name: 'Winter',
+    //   x: unpack(rows, 'getData.year'),
+    //   y: unpack(rows, 'getData.season'),
+    //   line: {color: '#7F7F7F'}
+    // }
+    
+    // var trace4 = {
+    //   type: "scatter",
+    //   mode: "lines",
+    //   name: 'Holiday Season',
+    //   x: unpack(rows, 'getData.year'),
+    //   y: unpack(rows, 'AAPL.Low'),
+    //   line: {color: '#7F7F7F'}
+    // }
+    
+    // var trace5 = {
+    //   type: "scatter",
+    //   mode: "lines",
+    //   name: 'Spring',
+    //   x: unpack(rows, 'Date'),
+    //   y: unpack(rows, 'AAPL.Low'),
+    //   line: {color: '#7F7F7F'}
+    // }
+    
+    
+    var data = [trace1];
+    
+    var layout = {
+      title: 'Basic Time Series',
+    };
+    
+    // Plotly.newPlot('dan-chart', data, layout);
+    
+    Plotly.newPlot('dan-chart', data, layout);
+
+
+
+    });
+
+
+
+
+// d3.csv("https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv", function(err, rows){
+
+  
+
+// })
